5b531d35229962ec7fd16f5a13f3dba9a41d5cbf
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-06-05 00:15:37 +0530

    project structure added

--- app/src/main/java/com/example/shopkaro/activites/MainActivity.kt
+++ app/src/main/java/com/example/shopkaro/activites/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.example.shopkaro
+ package com.example.shopkaro.activites
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shopkaro.R
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
